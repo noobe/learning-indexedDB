938d67aca7030bfc2f918abe8f4a11012dee3694
Sarath <sarath.regedit@gmail.com>
2022-11-06 23:48:23 +0530

    Script to check browser support

---
+++ checkSupport.js
@@ -0,0 +1,11 @@
+window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || 
+window.msIndexedDB;
+ 
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || 
+window.msIDBTransaction;
+window.IDBKeyRange = window.IDBKeyRange || 
+window.webkitIDBKeyRange || window.msIDBKeyRange
+ 
+if (!window.indexedDB) {
+   window.alert("Your browser doesn't support a stable version of IndexedDB.")
+}
